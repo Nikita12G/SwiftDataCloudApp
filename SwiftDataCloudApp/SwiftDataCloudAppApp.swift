3d01095bd60f1ad9e43acbeaebbf93e094f5e6f1
Nikita12G <gulyi08@mail.ru>
2024-09-03 17:06:11 +0700

    Initial Commit

---
+++ SwiftDataCloudApp/SwiftDataCloudAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataCloudAppApp.swift
+//  SwiftDataCloudApp
+//
+//  Created by Никита Гуляев on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataCloudAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
